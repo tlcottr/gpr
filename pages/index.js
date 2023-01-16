fc78d912e05fda11490cde90c4c66937d076d3f3
Taylor Cottrell <32750540+tlcottr@users.noreply.github.com>
2023-01-16 01:45:40 -0500

    add loader

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
 import Scene from "@/components/Scene";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { Loader } from "@react-three/drei";
 
 export default function Home() {
   return (
     <div>
+      <Loader />
       <Header />
       <Scene />
       <Footer />
